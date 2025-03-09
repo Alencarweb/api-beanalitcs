a59bdba7e8c68a93559267b834e622dfa4b35a95
Alencardev <alencarsouza2011@hotmail.com>
2025-03-09 16:34:11 -0300

    implementação s3, upload fotos e credenciais no .env

--- models/Banner.js
+++ models/Banner.js
@@ -7,9 +7,9 @@ const BannerSchema = new mongoose.Schema({
   data_start: { type: Date, required: true },
   data_end: { type: Date, required: true },
   link: { type: String, required: true },
-  tag_google: { type: String, required: false }, // Código do Google Tag Manager
+  tag_google: { type: String, required: false }, 
   tag_active: { type: Boolean, default: true },
-  banner_location: [{ type: mongoose.Schema.Types.ObjectId, ref: 'BannerLocation', required: true }], // Referência ao local do banner
+  banner_location: [{ type: mongoose.Schema.Types.ObjectId, ref: 'BannerLocation', required: true }], 
 });
 
 export default mongoose.model('Banner', BannerSchema);
--- routes/banner.js
+++ routes/banner.js
@@ -1,13 +1,44 @@
 import express from 'express';
 import Banner from '../models/Banner.js';
 import auth from '../middleware/auth.js';
+import multer from 'multer';
+import multerS3 from 'multer-s3';
+import { S3Client } from '@aws-sdk/client-s3';
+import 'dotenv/config';
+
 const router = express.Router();
+
+
+const s3 = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.AWS_BUCKET_NAME,
+    acl: 'public-read',
+    key: function (req, file, cb) {
+      const year = new Date().getFullYear();
+      const month = new Date().getMonth() + 1;
+      const fileName = `${year}/${month}/${Date.now()}_${file.originalname}`;
+      cb(null, `banners/${fileName}`);
+    },
+  }),
+});
+
 
 router.use(auth);
 
-router.post('/', async (req, res) => {
+router.post('/', upload.fields([{ name: 'imagem_desktop' }, { name: 'imagem_mobile' }]), async (req, res) => {
   try {
-    const { banner_name, imagem_desktop, imagem_mobile, data_start, data_end, link, tag_google, tag_active, banner_location } = req.body;
+    const { banner_name, data_start, data_end, link, tag_google, tag_active, banner_location } = req.body;
+    const imagem_desktop = req.files.imagem_desktop ? req.files.imagem_desktop[0].location : null;
+    const imagem_mobile = req.files.imagem_mobile ? req.files.imagem_mobile[0].location : null;
 
     const banner = new Banner({
       banner_name,
@@ -18,7 +49,7 @@ router.post('/', async (req, res) => {
       link,
       tag_google,
       tag_active,
-      banner_location,
+      banner_location:JSON.parse(banner_location),
     });
 
     await banner.save();
@@ -49,25 +80,26 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.put('/:id', async (req, res) => {
+router.put('/:id', upload.fields([{ name: 'imagem_desktop' }, { name: 'imagem_mobile' }]), async (req, res) => {
   try {
-    const { banner_name, imagem_desktop, imagem_mobile, data_start, data_end, link, tag_google, tag_active, banner_location } = req.body;
+    const { banner_name, data_start, data_end, link, tag_google, tag_active, banner_location } = req.body;
+    const imagem_desktop = req.files.imagem_desktop ? req.files.imagem_desktop[0].location : null;
+    const imagem_mobile = req.files.imagem_mobile ? req.files.imagem_mobile[0].location : null;
 
-    const banner = await Banner.findByIdAndUpdate(
-      req.params.id,
-      {
-        banner_name,
-        imagem_desktop,
-        imagem_mobile,
-        data_start,
-        data_end,
-        link,
-        tag_google,
-        tag_active,
-        banner_location,
-      },
-      { new: true } 
-    );
+    const updateData = {
+      banner_name,
+      data_start,
+      data_end,
+      link,
+      tag_google,
+      tag_active,
+      banner_location: JSON.parse(banner_location), // Parse the banner_location to an array
+    };
+
+    if (imagem_desktop) updateData.imagem_desktop = imagem_desktop;
+    if (imagem_mobile) updateData.imagem_mobile = imagem_mobile;
+
+    const banner = await Banner.findByIdAndUpdate(req.params.id, updateData, { new: true });
 
     if (!banner) {
       return res.status(404).send({ message: 'Banner não encontrado' });
